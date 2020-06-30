c88a0c8a4e8ca9cf2949dc54aacf3f39ac3229f8
Donald Luo <donaldluo@gmail.com>
2020-06-30 11:42:54 +0800

    Merge rbenv changes

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,8 +1,11 @@
 #!/usr/bin/env zsh
 
 rbenv_exec=$(command -v rbenv)
+brew_exec=$(command -v brew)
 
 if [ -x "$rbenv_exec" ] ; then
   eval "$(rbenv init -)"
-  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+  if [ -x "$brew_exec" ] ; then
+    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+  fi
 fi
